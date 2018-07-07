6dfb6fde5e58fa0475f4f950ac010e585e332804
TurtleByte <biehnjun@hotmail.com>
2018-07-07 09:06:55 +0800

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 	var oneDay = 24*60*60*1000;
-	var secondDate = new Date("2018-07-05T23:30:00Z");
-	var thirdDate = new Date("2018-07-06T23:30:00Z");
+	var secondDate = new Date("2018-07-06T23:30:00Z");
+	var thirdDate = new Date("2018-07-07T23:30:00Z");
 	
 	//Convert time to UTC from EDT (UTC -4), you useless pebble.
 	
